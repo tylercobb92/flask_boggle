9675bc0ff1dc1be2924a22e384561bfa0d8c94e3
tyler <tylercobb92@gmail.com>
2022-03-06 17:14:24 -0500

    / route now displays a 5x5 grid of letters

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+from flask import Flask, render_template, session
 from boggle import Boggle
 
 app = Flask(__name__)
-app.config['SECRET_KEY']="qwefasdgqwef5265aasdf"
+app.config['SECRET_KEY'] = "qwefasdgqwef5265aasdf"
 
 boggle_game = Boggle()
 
 
+@app.route('/')
+def home():
+    board = boggle_game.make_board()
+    session['board'] = board
+
+    return render_template("index.html", board=board)
