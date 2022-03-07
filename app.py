20f417dd3dc2566c6bec1d2ef56cc72f0e7a360f
tyler <tylercobb92@gmail.com>
2022-03-07 15:59:27 -0500

    added routes for checking submitted words, posting score

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, jsonify
 from boggle import Boggle
 
 app = Flask(__name__)
@@ -11,5 +11,30 @@ boggle_game = Boggle()
 def home():
     board = boggle_game.make_board()
     session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
 
-    return render_template("index.html", board=board)
+    return render_template("index.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
+
+
+@app.route('/check-word')
+def check_word():
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+
+@app.route('/post-score', methods=["POST"])
+def post_score():
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
